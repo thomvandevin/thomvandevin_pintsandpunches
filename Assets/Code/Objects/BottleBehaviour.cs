b9043e8b43daa738920d376e1a26ecb63124fb46
Daniel <danielbonnet.mail@gmail.com>
2014-11-18 18:35:21 +0100

    Bottle spawning and behaviour scripts made
    Made the scripts to spawn bottles and to destroy them.

---
+++ Assets/Code/Objects/BottleBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottleBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Check to see if screenshake was activated twice in the last 5 seconds. if yes, let the bottle drop (give rigidbody and let it drop)
+	}
+}
---
+++ Assets/Code/Objects/RandomBottle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomBottle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Randomly spawn one of the bottles
+		//grab a number from 1 to x (where xi s the amount of bottles) and spawn numbered bottle. 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
