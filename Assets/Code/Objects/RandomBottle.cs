4bc7725da65f8091f9a79d9d3fd6d2a96d43783e
Daniel <danielbonnet.mail@gmail.com>
2014-11-19 18:02:33 +0100

    Edited Paintings & Bottles
    Added paintings to the level and placed bottlespawners.

--- Assets/Code/Objects/RandomBottle.cs
+++ Assets/Code/Objects/RandomBottle.cs
@@ -4,18 +4,21 @@ using System.Collections;
 public class RandomBottle : MonoBehaviour {
 	
     //hier een privaye spriterenderer genaamd sprite
-
+	private SpriteRenderer sprite;
+	private int bottleNumber;
+	
 	// Use this for initialization
 	void Start () 
 	{
         ////Randomly spawn one of the bottles
         ////grab a number from 1 to x (where xi s the amount of bottles) and spawn numbered bottle. 
 
+		bottleNumber = Random.Range(1,17);
         //hier de spriterenderer koppelen, sprite = gameobject.getcomponent
-        //int voor random bottle, met random(0,17)
-
+		sprite = gameObject.GetComponent<SpriteRenderer>();
+		
         //sprite.sprite = pad naar image toe + randomint .ToString()
-
+		sprite.sprite = Resources.Load("/Sprites/Environment/Environment_01/Background/Bottles/Level_Pubcounter_Bottle_0" + bottleNumber.ToString("D2")) as Sprite;
 
 	}
 	
