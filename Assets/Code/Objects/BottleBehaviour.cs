7cc1e09275faef362a69b1d0459a21a3b87d4acd
Thom van de Vin (School) <pommee@live.nl>
2014-11-21 12:13:10 +0100

    BottleBehaviour fixed

--- Assets/Code/Objects/BottleBehaviour.cs
+++ Assets/Code/Objects/BottleBehaviour.cs
@@ -2,12 +2,18 @@
 using System.Collections;
 
 public class BottleBehaviour : MonoBehaviour {
-	
+
+    private SpriteRenderer sprite;
+    private int randomBottle;
+
 	// Use this for initialization
 	void Start () 
 	{
-		
-	}
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+        randomBottle = Random.Range(1,17);
+
+        sprite.sprite = Resources.Load("Sprites/Environment/Environment_01/Background/Bottles/Level_PubCounter_Bottle_0" + randomBottle.ToString("D2")) as Sprite;
+    }
 	
 	// Update is called once per frame
 	void Update () 
--- Assets/Code/Objects/RandomBottle.cs
+++ Assets/Code/Objects/RandomBottle.cs
@@ -3,23 +3,18 @@ using System.Collections;
 
 public class RandomBottle : MonoBehaviour {
 	
-    //hier een privaye spriterenderer genaamd sprite
 	private SpriteRenderer sprite;
+    private Sprite randomSprite;
 	private int bottleNumber;
 	
 	// Use this for initialization
 	void Start () 
 	{
-        ////Randomly spawn one of the bottles
-        ////grab a number from 1 to x (where xi s the amount of bottles) and spawn numbered bottle. 
-
+        sprite = gameObject.GetComponent<SpriteRenderer>();
 		bottleNumber = Random.Range(1,17);
-        //hier de spriterenderer koppelen, sprite = gameobject.getcomponent
-		sprite = gameObject.GetComponent<SpriteRenderer>();
-		
-        //sprite.sprite = pad naar image toe + randomint .ToString()
-		sprite.sprite = Resources.Load("/Sprites/Environment/Environment_01/Background/Bottles/Level_Pubcounter_Bottle_0" + bottleNumber.ToString("D2")) as Sprite;
 
+        randomSprite = Resources.Load<Sprite>("Sprites/Environment/Environment_01/Background/Bottles/Level_Pubcounter_Bottle_0" + bottleNumber.ToString("D2"));
+        sprite.sprite = randomSprite;
 	}
 	
 	// Update is called once per frame
