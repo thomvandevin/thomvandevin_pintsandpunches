d38ba3bbbfd3142c1a773240c906cbb7076fc8cb
Thom van de Vin (School) <pommee@live.nl>
2014-12-12 18:08:33 +0100

    Cabinet meta file

--- Assets/Code/Hardware/MPUInputCollector.cs
+++ Assets/Code/Hardware/MPUInputCollector.cs
@@ -15,7 +15,7 @@ public class MPUInputCollector : MonoBehaviour {
 	void Update () 
     {
         //print(digitalInputs[0] + " : " + digitalInputs[1] + " : " + digitalInputs[2] + " : " + digitalInputs[3] + " : " + digitalInputs[4] + " : " + digitalInputs[5]);
-        //print(analogInputs[0] + " : " + analogInputs[1] + " : " + analogInputs[2] + " : " + analogInputs[3] + " : " + analogInputs[4] + " : " + analogInputs[5]);
+        print(analogInputs[0] + " : " + analogInputs[1] + " : " + analogInputs[2] + " : " + analogInputs[3] + " : " + analogInputs[4] + " : " + analogInputs[5]);
     }
 
     public int GetGyroXValue()
